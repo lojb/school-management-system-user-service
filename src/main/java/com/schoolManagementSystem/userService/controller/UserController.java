9645795ebc8788b95478dbb92dd50190560fc1dd
lojb <balazsloj02@gmail.com>
2024-01-25 12:03:56 +0100

    Implement UserController with getAllUsers endpoint

--- src/main/java/com/schoolManagementSystem/userService/controller/UserController.java
+++ src/main/java/com/schoolManagementSystem/userService/controller/UserController.java
@@ -1,12 +1,24 @@
 package com.schoolManagementSystem.userService.controller;
 
+import com.schoolManagementSystem.userService.model.User;
+import com.schoolManagementSystem.userService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
-    public String getMessage() {
-        return "UserService endpoint";
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
