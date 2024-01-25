a7350059ecaf84fe26bff525fa0d151262a92bb3
lojb <balazsloj02@gmail.com>
2024-01-25 12:02:48 +0100

    Implement UserService with getAllUsers method

---
+++ src/main/java/com/schoolManagementSystem/userService/service/UserService.java
@@ -0,0 +1,24 @@
+package com.schoolManagementSystem.userService.service;
+
+import com.schoolManagementSystem.userService.model.User;
+import com.schoolManagementSystem.userService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
